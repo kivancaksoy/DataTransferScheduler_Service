156ebeffc0e75432c7cb218045300591bf6e3c53
kivancaksoy <kvnc96@hotmail.com>
2023-06-05 23:44:33 +0300

    initial commit

---
+++ src/test/java/scheduledExternal/scheduledService/ScheduledServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package scheduledExternal.scheduledService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
